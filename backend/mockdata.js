2887bd0b81ffd7492839ac9faf2cc37b8341c61d
Hustle999 <tuguutuugii02@gmail.com>
2024-12-27 13:51:11 +0800

    very well

---
+++ backend/mockdata.js
@@ -0,0 +1,20 @@
+export const MockData = [
+  {
+    title: "uzeg",
+    price: "5000",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDs3ZsrkiwNSoXpHynpGJ03l8k7LNvitQBHQ&s",
+  },
+  {
+    title: "uzeg2",
+    price: "5000",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDs3ZsrkiwNSoXpHynpGJ03l8k7LNvitQBHQ&s",
+  },
+  {
+    title: "uze3",
+    price: "5000",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDs3ZsrkiwNSoXpHynpGJ03l8k7LNvitQBHQ&s",
+  },
+];
